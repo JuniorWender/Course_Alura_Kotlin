9d1e6f17025afbdbf61b0d378438f5282f1ecc55
wender Junior <wender.junior@etaure.com.br>
2022-08-10 09:01:15 -0300

    Criando Classe Endereco e adicionando Composição com a Classe Cliente

--- src/main/kotlin/Alura_Kotlin/JuniorWender/GitHub/modelo/Cliente.kt
+++ src/main/kotlin/Alura_Kotlin/JuniorWender/GitHub/modelo/Cliente.kt
@@ -3,6 +3,7 @@ package Alura_Kotlin.JuniorWender.GitHub.modelo
 class Cliente(
     var nome: String,
     val cpf: String,
+    var Endereco: Endereco = Endereco(),
     private val senha: Int
 ) : Autenticavel {
 
---
+++ src/main/kotlin/Alura_Kotlin/JuniorWender/GitHub/modelo/Endereco.kt
@@ -0,0 +1,14 @@
+package Alura_Kotlin.JuniorWender.GitHub.modelo
+
+class Endereco(
+    var logradouro: String = "",
+    var numero: Int = 0,
+    var bairro: String = "",
+    var cidade: String = "",
+    var estado: String = "",
+    var cep: String = "",
+    var complemento: String = ""
+) {
+
+
+}
