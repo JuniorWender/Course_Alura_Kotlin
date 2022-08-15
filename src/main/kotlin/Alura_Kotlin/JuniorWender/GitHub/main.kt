e9639314d8ec7eb4d97cf549d2505f5b693432e4
wender Junior <wender.junior@etaure.com.br>
2022-08-15 13:17:11 -0300

    Sobreescrevendo metodo equals, toString e HashCode na Classe Endereco

--- src/main/kotlin/Alura_Kotlin/JuniorWender/GitHub/main.kt
+++ src/main/kotlin/Alura_Kotlin/JuniorWender/GitHub/main.kt
@@ -3,10 +3,12 @@ import Alura_Kotlin.JuniorWender.GitHub.modelo.Endereco
 fun main() {
     println("Bem vindo ao bytebank")
 
-    val endereco = Endereco()
+    val endereco = Endereco(logradouro = "Rua x", complemento = "teste", numero= 1000, cep = "00000-000")
 
-    val enderecoNovo = Endereco()
+    val enderecoNovo = Endereco(logradouro = "Rua y", complemento = "n/a", numero= 1000, cep = "00000-000")
 
     println(endereco)
+
+    print(endereco.equals(enderecoNovo))
 
 }
